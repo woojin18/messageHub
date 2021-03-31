a4b65e3f1c9acc88807d567dcd802fd21d831a12
liam <liam@DESKTOP-V69QNLM>
2021-03-31 13:10:36 +0900

    푸시 메세지 발송 신규등록

---
+++ src/main/java/kr/co/uplus/cloud/sendMessage/controller/SendMessageController.java
@@ -0,0 +1,56 @@
+package kr.co.uplus.cloud.sendMessage.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.uplus.cloud.sample.dto.RestResult;
+import kr.co.uplus.cloud.sendMessage.service.SendMessageService;
+
+/**
+ * <pre>
+ * SendMessageController
+ * 각 채널별 메시지 발송처리 Controller
+ * </pre>
+ *
+ * @author : liam (Lee Sung Min)
+ * @Date : 2021.03.29.
+ * @Version : 1.0 Copyright 2020 LG Uplus Corp. All Rights Reserved.
+ */
+@RestController
+@RequestMapping("/send/message")
+public class SendMessageController {
+
+	@Autowired
+	private SendMessageService sendMsgService;
+
+	/**
+	 * 푸시 메시지 발송처리
+	 * 
+	 * @param request
+	 * @param response
+	 * @param params
+	 * @return
+	 */
+	@PostMapping("/push")
+	public RestResult<?> sendPushMessage(HttpServletRequest request, HttpServletResponse response,
+			@RequestBody Map<String, Object> params) {
+		RestResult<Object> rtn = new RestResult<Object>();
+		try {
+			rtn = sendMsgService.sendPushMessage(params);
+		} catch (Exception e) {
+			rtn.setSuccess(false);
+			rtn.setMessage("실패하였습니다.");
+		}
+
+		return rtn;
+	}
+
+}
---
+++ src/main/java/kr/co/uplus/cloud/sendMessage/service/SendMessageService.java
@@ -0,0 +1,55 @@
+package kr.co.uplus.cloud.sendMessage.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.uplus.cloud.sample.dto.RestResult;
+import kr.co.uplus.cloud.utils.GeneralDao;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * <pre>
+ * SendMessageService
+ * 각 채널별 메시지 발송처리 Service
+ * </pre>
+ * 
+ * @author liam (Lee Sung Min)
+ * @Date : 2021.03.29.
+ * @Version : 1.0 Copyright 2020 LG Uplus Corp. All Rights Reserved.
+ */
+@Log4j2
+@Service
+public class SendMessageService {
+
+	@Autowired
+	private GeneralDao generalDao;
+
+	/**
+	 * 푸시 메시지 발송처리
+	 * 
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
+	public RestResult<Object> sendPushMessage(Map<String, Object> params) throws Exception {
+
+		RestResult<Object> rtn = new RestResult<Object>();
+
+		// 푸시 발송 G/W API 호출
+
+		// 푸시 발송내역 저장
+		int resultCnt = generalDao.insertGernal("sendMessage.qryInsertSendPushMessage", params);
+
+		if (resultCnt <= 0) {
+			rtn.setSuccess(false);
+			rtn.setMessage("실패하였습니다.");
+		} else {
+			rtn.setSuccess(true);
+		}
+
+		return rtn;
+	}
+}
