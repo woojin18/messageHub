af3de3ad6d736bcdb12ac0aa4335d5c3fcd11ff9
liam <liam@DESKTOP-V69QNLM>
2021-05-21 18:00:13 +0900

    개발환경 설정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,46 +1,45 @@
 module.exports = {
-  /** build config */
-  outputDir: process.env.NODE_ENV === 'production'
-    ? '../src/main/resources/templates' : 'dist', // html, ico 파일 저장
-  assetsDir: process.env.NODE_ENV === 'production'
-    ? '../static' : '', // css, img, js 저장. outputDir에 대한 상대 경로
-
-  /** 
-   * /api/** 요청은 backend 서버로 전달한다.
-   * 이 설정이 없으면 axios에서 baseURL 설정하고 backend에서는 cors 설정 해야한다.
-   */
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/listCtl': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/projectApi': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/userApi': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/messageStatusApi': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/integratedTemplateApi': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      },
-      '^/addressApi': {
-        target: 'http://localhost:7070',
-        changeOrigin: true
-      }
-    }
-  },
-
-  runtimeCompiler: true,
+	/** build config */
+	outputDir: '../src/main/resources/static', // html, ico 파일 저장
+	publicPath: '/',
+	assetsDir: 'assets', // css, img, js 저장. outputDir에 대한 상대 경로
+	productionSourceMap: undefined,
+	/** 
+	 * /api/** 요청은 backend 서버로 전달한다.
+	 * 이 설정이 없으면 axios에서 baseURL 설정하고 backend에서는 cors 설정 해야한다.
+	 */
+	devServer: {
+		port: process.env.VUE_APP_PORT || 3000,
+		proxy: {
+			'^/api': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/listCtl': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/projectApi': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/userApi': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/messageStatusApi': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/integratedTemplateApi': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			},
+			'^/addressApi': {
+				target: process.env.VUE_APP_TARGET_URL || 'http://localhost:7070',
+				changeOrigin: true
+			}
+		}
+	},
+	runtimeCompiler: true,
 }
