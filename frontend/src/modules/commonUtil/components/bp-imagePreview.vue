1476dfb3370b4a3c89bc6ede155bee801ef886c2
jj04h <jj04h@Jang>
2021-06-01 16:38:55 +0900

    이미지 미리보기 최대 사이즈와 가운데 정렬 기능 추가

--- frontend/src/modules/commonUtil/components/bp-imagePreview.vue
+++ frontend/src/modules/commonUtil/components/bp-imagePreview.vue
@@ -2,8 +2,8 @@
   <div v-if="imgPreviewOpen" @click.self="fnClose" class="modalStyle" id="image-add" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog" style="width:785px">
       <div class="modal-content">
-        <div class="modal-body">
-          <img :src="imageUrl" style="top:0;left:0;width:100%;height:100%;"/>
+        <div class="modal-body" style="text-align: center;">
+          <img :src="imageUrl" style="top:0;left:0;max-width: 100%;height:auto;"/>
         </div>
         <div class="text-center mt10 mb10">
           <a @click="fnClose" class="btnStyle3 white font14" data-dismiss="modal" title="닫기">닫기</a>
