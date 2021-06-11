b3e62841796f963135382e49952da622efd946f9
liam <liam@DESKTOP-V69QNLM>
2021-06-11 13:40:37 +0900

    main page 수정

--- frontend/src/components/PublicHeader.vue
+++ frontend/src/components/PublicHeader.vue
@@ -1,50 +1,78 @@
 <template>
-  <!-- head_type_user -->
-  <header class="head_type_user">
-    <div class="wrap_head">
-      <div class="box_gnb">
-        <h1 class="tit_logo"><a href="/main" class="userLogo"><img src="../../public/se2/images/userLogo.svg" alt="LGU+ 통합메시징클라우드 로고"></a></h1>
-        <nav id="userGnb">
-          <ul class="list_gnb" role="menu">
-            <li>
-              <router-link :to="{name: 'intro'}" tag="a" title="서비스 소개 페이지로 이동">서비스 소개</router-link>
-            </li>
-            <li>
-              <router-link :to="{name: 'sendPay'}" tag="a" title="요금제 페이지로 이동">요금제</router-link>
-            </li>
-            <li><a href="#self" title="고객센터 페이지로 이동">고객센터</a>
-              <ul class="dep2">
-                <li><a href="#self" title="FAQ로 이동">FAQ</a></li>
-                <li><a href="#self">공지사항</a></li>
-                <li><a href="#self">자료실</a></li>
-                <li><a href="#self" data-toggle="modal" data-target="#Inquiry">서비스 문의</a></li>
-              </ul>
-            </li>
-            <li><a href="#self" title="사용자 가이드 페이지로 이동">사용자 가이드</a></li>
-            <li class="console"><a href="#self" title="CONSOLE 페이지로 이동">CONSOLE</a></li>
-          </ul>
-        </nav>
-        <ul class="list_login">
-          <li class="active">
-            <!-- <a @click.prevent="clickLoginPage">로그인</a> -->
-            <router-link :to="{name: 'login'}" tag="a" title="로그인 페이지로 이동">로그인</router-link>
-          </li>
-          <li><a @click.prevent="signUp">회원가입</a></li>
-        </ul>
-      </div>
-    </div>
-  </header>
-  <!-- //head_type_user -->
+	<!-- head_type_user -->
+	<header class="head_type_user">
+		<div class="wrap_head">
+			<div class="box_gnb">
+				<h1 class="tit_logo"><router-link to="/main" class="userLogo"><img src="../../public/se2/images/userLogo.svg" alt="LGU+ 통합메시징클라우드 로고"></router-link></h1>
+				<nav id="userGnb">
+					<ul class="list_gnb" role="menu">
+						<li>
+							<router-link :to="{name: 'intro'}" tag="a" title="서비스 소개 페이지로 이동">서비스 소개</router-link>
+						</li>
+						<li>
+							<router-link :to="{name: 'sendPay'}" tag="a" title="요금제 페이지로 이동">요금제</router-link>
+						</li>
+						<li><a href="#self" title="고객센터 페이지로 이동">고객센터</a>
+							<ul class="dep2">
+								<li><a href="#self" title="FAQ로 이동">FAQ</a></li>
+								<li><a href="#self">공지사항</a></li>
+								<li><a href="#self">자료실</a></li>
+								<li><a href="#self" data-toggle="modal" data-target="#Inquiry">서비스 문의</a></li>
+							</ul>
+						</li>
+						<li><a href="#self" title="사용자 가이드 페이지로 이동">사용자 가이드</a></li>
+						<li class="console"><router-link to="/ac/home" title="CONSOLE 페이지로 이동">CONSOLE</router-link></li>
+					</ul>
+				</nav>
+				<ul class="list_login">
+					<li v-if="isLogin == false" class="active"><router-link :to="{name: 'login'}" tag="a" title="로그인 페이지로 이동">로그인</router-link></li>
+					<li v-if="isLogin == true" class="active"><a @click.prevent="clickLogout">로그아웃</a></li>
+					<li><a @click.prevent="signUp">회원가입</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+	<!-- //head_type_user -->
 </template>
+
 <script>
+import loginApi from '@/modules/login/service/api';
+import tokenSvc from '@/common/token-service';
+
 export default {
-  methods: {
-    signUp: function() {
-      this.$router.push({name : "signUp"});
-    },
-    clickLoginPage() {
-      this.$nextTick(() => this.$router.replace('/login'));
-    }
-  }
+	name: "PublicHeader",
+	data() {
+		return {
+			isLogin: false
+		}
+	},
+	created() {
+		this.isLogin = !!tokenSvc.getToken();
+		let curPage = location.pathname;
+		if (this.isLogin && curPage != "/login") {
+			this.$store.commit("login/isLogin", true);
+		} else {
+			this.$store.commit("login/isLogin", false);
+			this.isLogin = false;
+		}
+	},
+	methods: {
+		signUp: function() {
+			this.$router.push({name : "signUp"});
+		},
+		clickLoginPage() {
+			this.$nextTick(() => this.$router.replace('/login'));
+		},
+		clickLogout() {
+			let result = confirm("로그아웃 하시겠습니까?");
+			if (result) {
+				loginApi.logout().then(response => {
+					if (response.data.success) {
+						this.$router.go(this.$router.currentRoute);
+					}
+				});
+			}
+		},
+	}
 };
 </script>
