3ecc5e083ffac51e14cafc0154b86d9702505f28
jj04h <jj04h@Jang>
2021-07-02 09:53:19 +0900

    web_개발_수정사항 => 조회시 건수 옆에 Please select one 삭제 적용

--- frontend/src/components/SelectLayer.vue
+++ frontend/src/components/SelectLayer.vue
@@ -1,6 +1,6 @@
 <template>
     <select @change="fnSelectValue" v-model="selected" :class="classProps">
-        <option disabled value="">Please select one</option>
+        <!-- <option disabled value="">Please select one</option> -->
         <option value="10">10개씩 보기</option>
         <option value="20">20개씩 보기</option>
         <option value="30">30개씩 보기</option>
