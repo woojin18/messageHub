7eb1dec4d4a806630b4a3d1c80d403dcfc2ac016
bitcube10 <bitcube10@DESKTOP-KIRLLA9>
2021-10-01 10:11:20 +0900

    서비스 노출 순서 병경

--- src/main/java/kr/co/uplus/cm/project/service/ProjectService.java
+++ src/main/java/kr/co/uplus/cm/project/service/ProjectService.java
@@ -62,10 +62,10 @@ public class ProjectService {
 			
 			String useChStr = "";
 			
-			if( "Y".equals(rcsYn) )		useChStr += "RCS,";
 			if( "Y".equals(smsmmsYn) )	useChStr += "SMS/MMS,";
-			if( "Y".equals(pushYn) )	useChStr += "PUSH,";
+			if( "Y".equals(rcsYn) )		useChStr += "RCS,";
 			if( "Y".equals(kakaoYn) )	useChStr += "KAKAO,";
+			if( "Y".equals(pushYn) )	useChStr += "PUSH,";
 			if( "Y".equals(moYn) )		useChStr += "MO,";
 			
 			if( useChStr.length() > 0 ) {
