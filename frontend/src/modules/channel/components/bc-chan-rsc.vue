1e9aeec0a252940db0d1f3d8f384b204a422d49f
tpflv <tpflv@DESKTOP-VJUS0J3>
2021-10-07 15:56:56 +0900

    프로젝트 - 채널관리 - RCS 브랜드관리정보 수정보완

--- frontend/src/modules/channel/components/bc-chan-rsc.vue
+++ frontend/src/modules/channel/components/bc-chan-rsc.vue
@@ -13,10 +13,17 @@
     
 		<article>
       <!-- 검색창 -->
-			<div class="Dashboard01 border-line">
+			<div class="Dashboard01 border-line" style="padding-top: 30px;">
 				<div class="row">
 					<div class="col-xs-12">
-						<div class="menuBox">						
+            <p class="color3">
+              <i class="far fa-info-circle"></i> 브랜드를 등록하기 위해서는 RCS Biz Center(RBC)에 등록이 되어 있어야 합니다.<br>
+							<i class="far fa-info-circle"></i> RBC 홈페이지(https://www.rcsbizcenter.com)에서 무료로 만들 수 있습니다.<br>
+							<i class="far fa-info-circle"></i> RBC에 로그인 후 우측 상단 [내 정보관리]에서 API Key를 확인하십시오<br>
+							&nbsp;&nbsp;&nbsp;&nbsp;(브랜드 등록 및 수정 시 API Key[RBC 로그인 ID]와 API Secret Key[RBC API Key]를 사용합니다.)
+						</p>
+
+						<div class="menuBox mt10">						
 							<div>
                 <select id="srcBrandType" v-model="srcBrandType" class="selectStyle2" style="width:20%">
                   <option value="brandId">브랜드 아이디</option>
@@ -87,7 +94,10 @@
                   <td style="text-decoration: underline; cursor: pointer;">
                     <a @click="fnRcsCallbackDetail(row)">{{ row.callbackCnt }}</a>
                   </td>
-                  <td>
+                  <td v-if="row.approvalStatus == '반려'" style="text-decoration: underline; cursor: pointer;">
+                    <a @click="fnAlertReason(row.reason)">{{ row.approvalStatus }}</a>
+                  </td>
+                  <td v-else>
                     {{ row.approvalStatus }}
                   </td>
                   <td>
@@ -309,6 +319,10 @@ export default {
       this.callback_row_data = row;
       jQuery("#confirmCallback").modal("show");
     },
+
+    fnAlertReason(reason) {
+      confirm.fnAlert("", reason);
+    }
   }
 }
 </script>
