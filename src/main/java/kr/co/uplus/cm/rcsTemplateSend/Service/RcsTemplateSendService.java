5c209d16c321ff9c0afbe2a1714b7f9a14d11abd
seo <seo@DESKTOP-4E9JLO0>
2021-10-14 17:01:37 +0900

    commit

--- src/main/java/kr/co/uplus/cm/rcsTemplateSend/Service/RcsTemplateSendService.java
+++ src/main/java/kr/co/uplus/cm/rcsTemplateSend/Service/RcsTemplateSendService.java
@@ -1699,7 +1699,8 @@ public class RcsTemplateSendService {
 			Date rsrvDate = dateFormat.parse(rsrvDateStr);
 			Date currentDate = new Date();
 
-			currentDate = DateUtils.addMinutes(currentDate, 10);
+			currentDate = DateUtils.addMinutes(currentDate, 9);
+			
 			if(currentDate.compareTo(rsrvDate) > 0) {
 				throw new Exception("잘못된 예약시간입니다. 현재시간 10분 이후로 설정해주세요.");
 			}
