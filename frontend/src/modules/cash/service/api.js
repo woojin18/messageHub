8e8f2bd0af26de1c4b8220903da966c6fb4519ac
Jimmy <Jimmy@KKBUM2000>
2022-01-24 19:59:45 +0900

    작업중

--- frontend/src/modules/cash/service/api.js
+++ frontend/src/modules/cash/service/api.js
@@ -65,6 +65,28 @@ const delDept = (params) => {
     return httpClient.post('/projectApi/cash/delDept', params, { headers: {"show-layer": "Yes", "activity":"SAVE"} });
 };
 
+const excelDownloadCalList = (params) => {
+	return httpClient.post('/projectApi/cash/excelDownloadCalList', params, { headers: {"show-layer": "No", "activity":"READ"}, responseType: 'arraybuffer' }).then(function(response) {
+		try {
+			let blob = new Blob([response.data], { type: response.headers['content-type'] })
+			let fileName = getFileName(response.headers['content-disposition'])
+			fileName = decodeURI(fileName)
+
+		if (window.navigator.msSaveOrOpenBlob) { // IE 10+
+			window.navigator.msSaveOrOpenBlob(blob, fileName)
+		} else { // not IE
+				let link = document.createElement('a')
+				link.href = window.URL.createObjectURL(blob)
+				link.target = '_self'
+				if (fileName) link.download = fileName
+				link.click()
+			}
+		} catch (e) {
+			console.error(e)
+		}
+	});
+};
+
 export default {
     insertWebCashInfo,
     selectCashHist,
@@ -81,5 +103,21 @@ export default {
     selectDeptList,
     selectBillList,
     saveDept,
-    delDept
+    delDept,
+    excelDownloadCalList
 };
+
+
+function getFileName (contentDisposition) {
+	let fileName = contentDisposition
+		.split(';')
+		.filter((ele) => {
+		return ele.indexOf('filename') > -1
+	})
+	.map((ele) => {
+		return ele
+			.replace(/"/g, '')
+			.split('=')[1]
+	})
+	return fileName[0] ? fileName[0] : null
+}
