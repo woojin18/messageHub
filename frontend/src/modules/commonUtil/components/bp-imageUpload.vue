86eca1c32e07ce9d3392780bfcb351ff9063e5d3
장동진 <jdwls27@bitcube.co.kr>
2022-03-08 15:56:20 +0900

    채널 별 상단 문구 추가 및 이미지 업로드 validation 수정

--- frontend/src/modules/commonUtil/components/bp-imageUpload.vue
+++ frontend/src/modules/commonUtil/components/bp-imageUpload.vue
@@ -5,24 +5,73 @@
         <div class="modal-body">
           <h2>이미지 업로드</h2>
           <hr>
+          <div class="border-line2 bgColor_f7 mt10 pd10">
+						<ul class="color5">
+							<li>•  이미지를 사용하실 채널을 선택해 주십시오.(채널마다 이미지 형식이 다를 수 있습니다.)</li>
+							<li>
+								<ul class="color5 ml10">
+									<li v-if="!this.chkboxUseCh.includes('MMS') && this.chkboxUseCh.length > 0">
+                    -이미지는 jpg, jpeg, png 형식만 지원합니다.
+                  </li>
+ 									<li v-else-if="this.chkboxUseCh.includes('MMS')">
+                    -이미지는 jpg, jpeg 형식만 지원합니다.
+                  </li>                         
+                  <li v-if="this.chkboxUseCh.includes('MMS')">
+                    -이미지 용량은 300KB 이하여야 합니다.
+                  </li>
+                   <li v-else-if="this.chkboxUseCh.includes('FRIENDTALK') && this.wideYn == 'N'">
+                    -이미지 용량은 500KB 이하여야 합니다.
+                  </li>   
+                   <li v-else-if="this.chkboxUseCh.includes('RCS') || this.chkboxUseCh.includes('PUSH')">
+                    -이미지 용량은 1MB 이하여야 합니다.
+                  </li>
+                   <li v-else-if="this.chkboxUseCh.includes('FRIENDTALK') && this.wideYn == 'Y'">
+                     -이미지 용량은 2MB 이하여야 합니다.
+                  </li>   
+
+								</ul>
+							</li>
+						</ul>
+					</div>
           <div class="of_h">
             <h4>이미지 사용채널</h4>
           </div>
           <div class="consolCheck" style="width:100%">
-            <input type="checkbox" id="imgPush" class="checkStyle2" value="PUSH" v-model="chkboxUseCh">
-            <label for="imgPush" class="mr10">푸시</label>
-            <input type="checkbox" id="imgRCS" class="checkStyle2" value="RCS" v-model="chkboxUseCh">
-            <label for="imgRCS" class="mr10">RCS</label>
-            <input type="checkbox" id="imgtalk" class="checkStyle2" value="FRIENDTALK" v-model="chkboxUseCh">
-            <label for="imgtalk" class="mr10">친구톡</label>
+            <input type="checkbox" id="imgMMS1" class="checkStyle2" value="MMS" v-model="chkboxUseCh">
+            <label for="imgMMS1" class="mr20 vertical-middle clear">MMS
+						<i class="fas fa-question-circle toolTip"><span class="toolTipText" style="width:330px">
+						* 이미지는 jpg, jpeg 형식만 지원합니다.<br>
+						* 이미지 용량은 300KB 이하여야 합니다.<br>
+						* 이미지 권장 사이즈는 320*240 또는 440*330 입니다.</span></i></label>
 
-            <select v-if="chkboxUseCh.includes('FRIENDTALK')" v-model="wideYn" class="selectStyle1 vertical-middle mr10" style="width:15%">
+            <input type="checkbox" id="imgRCS" class="checkStyle2" value="RCS" v-model="chkboxUseCh">
+            <label for="imgRCS" class="mr20 vertical-middle clear">RCS
+              <i class="fas fa-question-circle toolTip"><span class="toolTipText" style="width:330px">
+						* 이미지는 jpg, jpeg, png 형식만 지원합니다.<br>
+						* 이미지 용량은 1MB 이하여야 합니다.<br>
+						* 이미지 권장 사이즈는 568*336 또는 568*528 입니다.</span></i></label>
+
+            <input type="checkbox" id="imgtalk" class="checkStyle2" value="FRIENDTALK" v-model="chkboxUseCh">
+            <label for="imgtalk" class="mr10 vertical-middle clear">친구톡
+            	<i class="fas fa-question-circle toolTip"><span class="toolTipText" style="width:475px">
+						* 이미지는 jpg, jpeg, png 형식만 지원합니다.<br>
+						* 이미지 용량은 Normal은 500KB 이하, Wide는 2MB 이하여야 합니다.<br>
+						* Normal 이미지 권장 사이즈는 가로 길이 최소 720px, 세로 길이는 최소 720px 이상입니다.<br>
+						* Wide 이미지 권장 사이즈는 가로 길이 최소 800px, 세로 길이는 최소 600px 이상입니다.<br>
+						* 이미지 제한 사이즈는 가로:세로 비율 2:1 이상, 4:3 이하여야 합니다.</span></i></label>
+
+            <select v-if="chkboxUseCh.includes('FRIENDTALK')" v-model="wideYn" class="selectStyle2 mr20 vertical-middle" style="width:100px">
               <option value="N">Normal</option>
               <option value="Y">Wide</option>
             </select>
 
-            <input type="checkbox" id="imgMMS1" class="checkStyle2" value="MMS" v-model="chkboxUseCh">
-            <label for="imgMMS1" class="mr10">MMS</label>
+            <input type="checkbox" id="imgPush" class="checkStyle2" value="PUSH" v-model="chkboxUseCh">
+            <label for="imgPush" class="mr20 vertical-middle clear">푸시
+              <i class="fas fa-question-circle toolTip"><span class="toolTipText" style="width:330px">
+						* 이미지는 jpg, jpeg, png 형식만 지원합니다.<br>
+						* 이미지 용량은 1MB 이하여야 합니다.<br>
+						* 이미지 권장 사이즈는 568*336 또는 568*528 입니다.</span></i></label>
+
           </div>
 
           <div class="border-line mt10" style="width:100%;">
@@ -120,6 +169,39 @@ export default {
         confirm.fnAlert(this.componentsTitle, '허용되지 않는 확장자입니다.\n(이미지는 .jpg, .jpeg, .png(MMS 제외) 확장자 지원합니다.)');
         return;
       }
+      const img = new Image()
+      img.src= this.imageUrl
+
+      const imgRatio = img.height / img.width
+
+      if(this.chkboxUseCh.includes('MMS')){
+         if(uploadFile.files[0].size>307200){
+             confirm.fnAlert(this.componentsTitle, '이미지 용량은 300KB 이하여야 합니다.');
+             return;
+         }
+       }else if(this.chkboxUseCh.includes('FRIENDTALK') && this.wideYn == 'N'){
+          if(uploadFile.files[0].size>512000){
+             confirm.fnAlert(this.componentsTitle, '이미지 용량은 500KB 이하여야 합니다.');
+             return;
+          }
+          if(imgRatio < 0.5 || imgRatio > 0.75){
+            confirm.fnAlert(this.componentsTitle, '이미지 비율은 가로:세로 2:1 이상, 4:3 이하여야 합니다.');
+          }
+        }else if(this.chkboxUseCh.includes('RCS') || this.chkboxUseCh.includes('PUSH')){
+          if(uploadFile.files[0].size>1048576){
+             confirm.fnAlert(this.componentsTitle, '이미지 용량은 1MB 이하여야 합니다.');
+             return;
+          }
+        }
+        else if(this.chkboxUseCh.includes('FRIENDTALK') && this.wideYn == 'Y'){
+          if(uploadFile.files[0].size>2097152){
+            confirm.fnAlert(this.componentsTitle, '이미지 용량은 2MB 이하여야 합니다.');
+            return;
+          }
+          if(imgRatio < 0.5 || imgRatio > 0.75){
+            confirm.fnAlert(this.componentsTitle, '이미지 비율은 가로:세로 2:1 이상, 4:3 이하여야 합니다.');
+          }
+        }
 
       const uploadInfo = {
         useCh: this.chkboxUseCh,
@@ -132,6 +214,7 @@ export default {
 
       this.inProgress = true;
       const vm = this;
+
       await commonUtilApi.uploadImage(fd).then(response =>{
         this.inProgress = false;
         const result = response.data;
@@ -149,6 +232,7 @@ export default {
       }).catch(function () {
         vm.inProgress = false;
       });
+
     },
     //파일찾기 클릭시
     fnFindImgFile() {
