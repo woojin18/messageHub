146a270bae4eb0a8dabc190113df2a2e012408ad
장동진 <jdwls27@bitcube.co.kr>
2022-04-01 09:11:28 +0900

    build

--- frontend/src/modules/urlInfo/components/shortenedUrlListPopup.vue
+++ frontend/src/modules/urlInfo/components/shortenedUrlListPopup.vue
@@ -74,9 +74,9 @@
                     :key="idx"
                   >
                     <td class="text-center">{{ item.rownum }}</td>  <!-- NO -->
-                    <td class="text-center">{{ item.title }}</td>   <!-- 제목 -->
+                    <td class="text-center" style="word-break: break-all;">{{ item.title }}</td>   <!-- 제목 -->
                     <td class="text-center">{{ item.urlId }}</td>   <!-- URL ID -->
-                    <td class="text-left">{{ item.orgUrl }}</td>    <!-- 원본 URL -->
+                    <td class="text-left" style="word-break: break-all;">{{ item.orgUrl }}</td>    <!-- 원본 URL -->
                     <td class="text-center">{{ $moment(item.regDt).format('YYYY.MM.DD') }}</td>   <!-- 등록일 -->
                     <td class="text-center">{{ $moment(item.expDt).format('YYYY.MM.DD') }}</td>   <!-- 만료일 -->
                     <td class="text-center end">
